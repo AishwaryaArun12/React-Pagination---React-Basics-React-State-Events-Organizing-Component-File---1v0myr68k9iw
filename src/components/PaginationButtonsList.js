48baff23544d0d9260676ca69ebde5111ecfe9b7
AishwaryaArun12 <92584479+AishwaryaArun12@users.noreply.github.com>
2022-08-06 23:47:26 +0530

    2 commit

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,27 +1,27 @@
-import React from 'react'
-const PaginationButtonsList = () => {
-const noOfPages=20;
-function createButtons(){
-    const buttonsArr=[];
-    for(let i=0;i<noOfPages;i++){
-        buttonsArr.push(<button id={'button-'(i+1)} value={i+1} onClick={buttonClick}>{i+1}</button>)
-        return buttonsArr;
+import React from "react";
+const PaginationButtonsList = (props) => {
+  const noOfPages = 20;
+  function createButtons() {
+    const buttonsArr = [];
+    for (let i = 0; i < noOfPages; i++) {
+      buttonsArr.push(
+        <button id={"button-"(i + 1)} value={i + 1} onClick={buttonClick}>
+          {i + 1}
+        </button>
+      );
+      return buttonsArr;
     }
-    function buttonClick(e){
-        const redButtons=document.querySelectorAll('.active-btn');
-        redButtons.forEach((button)=>{
-            button.classList.remove('active-btn')
-        })
-        const button=document.getElementById('button-'+e.target.value);
-        button.classList.add('active-btn');
-        props.handleChange(e);
+    function buttonClick(e) {
+      const redButtons = document.querySelectorAll(".active-btn");
+      redButtons.forEach((button) => {
+        button.classList.remove("active-btn");
+      });
+      const button = document.getElementById("button-" + e.target.value);
+      button.classList.add("active-btn");
+      props.handleChange(e);
     }
-}
-    return (
-        <div className="pagination-buttons-list">
-            {createButtons()}
-        </div>
-    )
-}
+  }
+  return <div className="pagination-buttons-list">{createButtons()}</div>;
+};
 
-export { PaginationButtonsList }
+export { PaginationButtonsList };
