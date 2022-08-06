498ead551bbc6644ae24735f946a5d9e0eb6292c
AishwaryaArun12 <jj4245888@gmail.com>
2022-08-06 21:30:41 +0530

    commited

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import {useState,useEffect} from 'react';
+import {fetchPosts} from '../api/fetchPosts';
+import {Post} from './Post';
+import { PaginationButtonsList } from './PaginationButtonsList';
 
 
 const PostList = () => {
-    
+    const [page, setPage]=useState(1);
+    const [post,setPost]=useState([]);
+    const [isLoading, setIsLoading]=useState(true);
+    const limit=5;
+    useEffect(()=>{
+        setIsLoading(true);
+        fetchPosts(page,limit).then((res)=>{
+            setIsLoading(false);
+            setPost([...res.data])
+        })
+    },[page]);
+    function handleChange(e){
+        setPage(e.target.value);
+    }
     return (
         <>
-
+        {isLoading&&<h1>Loading...</h1>}
+        {!isLoading&&post.map((item,i)=>{
+            <Post title={item.title} body={item.body} id={i}></Post>
+        })}
+        <PaginationButtonsList handleChange={handleChange}></PaginationButtonsList>
         </>
     )
 }
